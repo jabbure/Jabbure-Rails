e1345f97205117d9be96157abb9f0c601fd22637
Test <Test@abc.com>
2015-02-03 00:40:11 +0530

    Routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get 'test_connectivity', to: 'home#test_connectivity'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :tables
+    end
+  end 
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
